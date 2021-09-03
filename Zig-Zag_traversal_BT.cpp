ea7ebc4dcd7688eee2afdac8ad6099449741b31d
ankitksh81 <ankitksh81@gmail.com>
2021-09-04 00:43:38 +0530

    zig-zag level order traversal

--- Zig-Zag_traversal_BT.cpp
+++ Zig-Zag_traversal_BT.cpp
@@ -13,40 +13,40 @@
  */
 
 vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-	if(root == nullptr) {
-		return {};
-	}
-	vector<vector<int>> ans;
-	queue<TreeNode*> q;
-	bool left_to_right = true;
-	
-	q.push(root);
-	while(!q.empty()) {
-		int q_size = q.size();
-		vector<int> cur_level;
-		for(int i = 0; i < q_size; i++) {
-			auto node = q.front();
-			q.pop();
+    if(root == nullptr) {
+        return {};
+    }
+    vector<vector<int>> ans;
+    queue<TreeNode*> q;
+    bool left_to_right = true;
+    
+    q.push(root);
+    while(!q.empty()) {
+        int q_size = q.size();
+        vector<int> cur_level;
+        for(int i = 0; i < q_size; i++) {
+            auto node = q.front();
+            q.pop();
 
-			if(node->left != nullptr) {
-				q.push(node->left);
-			}
-			if(node->right != nullptr) {
-				q.push(node->right);
-			}
+            if(node->left != nullptr) {
+                q.push(node->left);
+            }
+            if(node->right != nullptr) {
+                q.push(node->right);
+            }
 
-			cur_level.push_back(node->val);
-		}
+            cur_level.push_back(node->val);
+        }
 
-		if(!left_to_right) {
-			reverse(cur_level.begin(), cur_level.end());
-			left_to_right = true;
-		}
-		else {
-			left_to_right = false;
-		}
-		ans.push_back(cur_level);
-	}
+        if(!left_to_right) {
+            reverse(cur_level.begin(), cur_level.end());
+            left_to_right = true;
+        }
+        else {
+            left_to_right = false;
+        }
+        ans.push_back(cur_level);
+    }
 
-	return ans;
+    return ans;
 }
