52d2bec1abe48f4d17560d3135b45143fba97f70
ankitksh81 <ankitksh81@gmail.com>
2021-09-07 18:09:13 +0530

    lowest common ancestor of BT

---
+++ Lowest_common_ancestor_BT.cpp
@@ -0,0 +1,32 @@
+// Lowest Common ancestor in a binary tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == nullptr || root == p || root == q) {
+        return root;
+    }
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if(left == nullptr) {
+        return right;
+    }
+    else if(right == nullptr) {
+        return left;
+    }
+    else {
+        return root;
+    }
+}
